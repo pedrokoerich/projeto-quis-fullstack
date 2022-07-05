b7375822608a8be4ad5c1d58662c8fac23c4942a
Pedro <pedro@incasa.local>
2022-07-05 16:25:19 -0300

    Iniciando projeto Quiz Fullstack

---
+++ js/css_questions.js
@@ -0,0 +1,105 @@
+let questions = [
+    {
+        question: 'No contexto do uso de CSS para aplicar estilos a elementos HTML numa página web, o seletor universal, que referencia todos os elementos, é identificado pelo símbolo: ',
+        options: [
+            '#',
+            '%',
+            '&',
+            '*',
+            '@'
+        ],
+        answer: 3
+    },
+    {
+        question: 'O trecho a seguir corresponde à definição de um(a): #principal { background: gray; }',    
+        options: [
+            'Classe CSS',
+            'Identificador CSS',
+            'Tag HTML',
+            'Classe JavaScript',
+            'Função JavaSCript'
+        ],
+        answer: 1
+    },
+    {
+        question: 'O Box model é como as propriedades de CSS compõem as dimensões, em que, além do width e do height, as propriedades border e padding também influenciam no resultado final.',
+        options: [
+            'Verdadeiro',
+            'Falso'
+        ],
+        answer: 0
+    },
+    {
+        question: 'No contexto do CSS numa página Web, assinale o script que estabelece que todos os elementos serão exibidos de acordo com a definição dos estilos. ',
+        options: [
+            'p { font-size: 20px; color: blue}',
+            '.p { font-size: 20px; color: blue}',
+            '#p { font-size: 20px; color: blue}',
+            '<p> { font-size: 20px; color: blue}',
+            ':p { font-size: 20px; color: blue}'
+        ],
+        answer: 0
+    },
+    {
+        question: 'No contexto da formatação de páginas Web, assinale o papel da propriedade padding no CSS em relação a um box.  ',
+        options: [
+            'Definir a altura e a largura.',
+            'Definir a espessura da área que separa as bordas do preenchimento.',
+            'Definir a espessura das margens externas. ',
+            'Definir a espessura e o estilo da borda.',
+            'Definir a unidade utilizada para estabelecer as dimensões.'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Analise o código CSS exibido a seguir: #par { text-align: center; color: red; } No HTML abrangido pelo comando, serão afetados por esse estilo todos os elementos HTML que:',
+        options: [
+            'contêm a string “par” no atributo name.',
+            'estão marcados pela classe “par”.',
+            'foram criados dinamicamente na montagem da página.',
+            'foram definidos como parágrafos.',
+            'têm o atributo id igual a “par”.'
+        ],
+        answer: 4
+    },
+    {
+        question: '',
+        options: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        answer: 7
+    },
+    {
+        question: '',
+        options: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        answer: 8
+    },
+    {
+        question: '',
+        options: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        answer: 9
+    },
+    {
+        question: '',
+        options: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        answer: 10
+    }
+]
---
+++ js/html_questions.js
@@ -0,0 +1,102 @@
+let questions = [
+    {
+        question: '',
+        options: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        answer: 1
+    },
+    {
+        question: '',
+        options: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        answer: 2
+    },
+    {
+        question: '',
+        options: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        answer: 3
+    },
+    {
+        question: '',
+        options: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        answer: 4
+    },
+    {
+        question: '',
+        options: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        answer: 5
+    },
+    {
+        question: '',
+        options: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        answer: 6
+    },
+    {
+        question: '',
+        options: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        answer: 7
+    },
+    {
+        question: '',
+        options: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        answer: 8
+    },
+    {
+        question: '',
+        options: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        answer: 9
+    },
+    {
+        question: '',
+        options: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        answer: 10
+    }
+]
