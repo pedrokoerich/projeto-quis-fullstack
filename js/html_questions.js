ca60313013a7b3b5c8654d1916b11585e12d7bfb
Pedro <pedro@incasa.local>
2022-07-06 11:42:46 -0300

    Inserção de Questões de HTML

--- js/html_questions.js
+++ js/html_questions.js
@@ -1,102 +1,103 @@
 let questions = [
     {
-        question: '',
+        question: '<html><img src="assets/img/question1.jpg" alt="Questão 1"></html>',
         options: [
-            '',
-            '',
-            '',
-            ''
-        ],
-        answer: 1
-    },
-    {
-        question: '',
-        options: [
-            '',
-            '',
-            '',
-            ''
-        ],
-        answer: 2
-    },
-    {
-        question: '',
-        options: [
-            '',
-            '',
-            '',
-            ''
+            'O código HTML está incompleto.',
+            'O Titulo será apresentado em negrito na tela.',
+            'A renderização irá depender de cada navegador.',
+            'A tag <head> está incompleta.'
         ],
         answer: 3
     },
     {
-        question: '',
+        question: 'Assinale a alternativa correta:',
         options: [
-            '',
-            '',
-            '',
-            ''
+            'HTML é um programa que favorece o desenvolvimento de arquivos CSS.',
+            'HTML e CSS são as tecnologias utilizadas na construção de páginas da Web: HTML (html e xhtml) para a estrutura, CSS para o estilo e layout, incluindo WebFonts.',
+            'Ambos são os conceitos fundamentais aplicados em dados abertos conectados.',
+            'CSS é uma linguagem de programação poderosa e requer HTML para seu funcionamento.'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Entre os novos elementos do HTML5, o elemento:',
+        options: [
+            '<mark> é o ponto de parada do cursor em qualquer parte da página HTML.',
+            '<progress> define o progresso de uma tarefa.',
+            '<meter> interpreta medições meteorológicas.',
+            '<figcaption> captura figuras.',
+            '<main> define a estrutura principal da linguagem C dentro da página HTML.'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Assinale a opção que apresenta a tag incluída na especificação do HTML5 que permite a reprodução de arquivos que contenham som ou música.',
+        options: [
+            '<phonic>',
+            '<img>',
+            '<sound>',
+            '<music>',
+            '<audio>'
         ],
         answer: 4
     },
     {
-        question: '',
+        question: 'HTML é uma linguagem de marcação que descreve a estrutura de um documento. Essa linguagem é utilizada para desenvolver páginas Web. Sobre o HTML seguem-se três afirmações:I - Páginas criadas em HTML sempre são visualizadas da mesma forma em navegadores diferentes, mesmo quando executados em plataformas de hardware diferentes. II - As páginas escritas em HTML são arquivos de texto no formato ASCII. III - O HTML possui um conjunto previamente definido de tags, no entanto o usuário pode criar as suas próprias tags. É correto apenas o que se afirma em',
         options: [
-            '',
-            '',
-            '',
-            ''
+            'I.',
+            'II.',
+            'III',
+            'I e II',
+            'II e III'
         ],
-        answer: 5
+        answer: 4
     },
     {
-        question: '',
+        question: 'A linguagem HTML é utilizada para formatar o conteúdo de uma página da Internet e pode ser editada com o uso de qualquer editor de texto.',
         options: [
-            '',
-            '',
-            '',
-            ''
+            'Verdadeiro',
+            'Falso'
         ],
-        answer: 6
+        answer: 0
     },
     {
-        question: '',
+        question: 'Um serviço disponibilizado na Internet por meio de um servidor web (www) faz uso de HTML.',
         options: [
-            '',
-            '',
-            '',
-            ''
+            'Verdadeiro',
+            'Falso'
         ],
-        answer: 7
+        answer: 0
     },
     {
-        question: '',
+        question: 'Efeitos similares em HTML são obtidos com as tags',
         options: [
-            '',
-            '',
-            '',
-            ''
+            '<b></b>, <strong></strong>',
+            '<b></b>, <sup></sup>',
+            '<sup></sup>, <strong></strong>',
+            '<sup></sup>, <sub></sub>'
         ],
-        answer: 8
+        answer: 0
     },
     {
-        question: '',
+        question: 'A principal linguagem utilizada em páginas na Internet é',
         options: [
-            '',
-            '',
-            '',
-            ''
+            'JavaScript',
+            'VBScript',
+            'DDL',
+            'HTTP',
+            'HTML'
         ],
-        answer: 9
+        answer: 4
     },
     {
-        question: '',
+        question: 'Em relação ao HTML, é incorreto afirmar:',
         options: [
-            '',
-            '',
-            '',
-            ''
+            'Não é uma linguagem de programação no sentido estrito',
+            'É usada nos browsers para apresentação das páginas',
+            'Seus programas rodam nos browsers',
+            'Permite aos browsers lidar com arquivos em vários formatos',
+            'É simples, de fácil e rápido aprendizado'
         ],
-        answer: 10
+        answer: 3
     }
 ]
